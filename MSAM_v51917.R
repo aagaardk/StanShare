e30dcb121c3c0f8a5bf1adab8e3d03b19aa1d6b2
curtisburkhalter <curtisburkhalter@gmail.com>
2017-05-24 11:08:12 -0400

    MSAM code file
    This file sets model parameters and reads in the necessary data file

--- MSAM_v51917.R
+++ MSAM_v51917.R
@@ -29,7 +29,7 @@ last <- sapply(1:dim(y)[1], function(i)
 y[is.na(y)] <- -1
 
 ## Parameters monitored
-params <- c("omega", "lambda", "alpha", "beta")
+params <- c("N","lambda","alpha","beta")
 
 ## MCMC settings
 ni <- 2000
@@ -39,10 +39,10 @@ nc <- 1
 
 ## Initial values
 inits <- lapply(1:nc, function(i)
-  list(lambda = runif(7, 0, 1)))
+  list(lambda = runif(7,1,1),N = matrix(runif(R*7,1,1),nrow = 95,ncol =7, byrow=TRUE),alphaP = runif(7,1,1), betaP = runif(7,1,1)))
 
 ## Call Stan from R
-out1 <- stan("Nmix_adapted.stan",
+out1 <- stan("Nmix_adapted_noZI.stan",
              data = list(y = y, R = R, T = 2,K = 100),
              init = inits, pars = params,
              chains = nc, iter = ni, warmup = nb, thin = nt,
